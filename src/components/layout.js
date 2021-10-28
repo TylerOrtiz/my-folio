0decf27406879f5772abbc3bcb40a8084925d7d6
Tyler O <ortizt89@gmail.com>
2021-10-27 23:00:50 -0400

    Integrate react icons.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,13 @@
 import React from "react"
+import { VscGithubInverted } from 'react-icons/vsc';
 
 export default function Layout({ children }) {
   return (
     <div className="container mx-auto">
+      <header className="flex justify-end">
+        
+        <a className="align-middle" href="https://github.com/TylerOrtiz"> <VscGithubInverted className="inline-block" /> <span className="align-text-top">TylerOrtiz</span></a>
+      </header>
       {children}
     </div>
   )
